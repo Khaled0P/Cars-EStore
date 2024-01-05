9287abbcba472c246cbbb4af184b0497e39d7451
Khaled AbdRahem <aa.khaled11@gmail.com>
2024-01-06 00:31:30 +0200

    chore: Remove unused icons and update footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,25 +1,24 @@
-import type { Metadata } from 'next'
-import './globals.css'
-import { Footer, NavBar } from '@/components'
-
+import type { Metadata } from 'next';
+import './globals.css';
+import { Footer, NavBar } from '@/components';
 
 export const metadata: Metadata = {
-  title: 'Cars E-Store',
+  title: 'CarsHub',
   description: 'Discover the best cars in the world',
-}
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className='relative'>
+      <body className="relative">
         <NavBar />
         {children}
         <Footer />
       </body>
     </html>
-  )
+  );
 }
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,32 +1,45 @@
-import Image from "next/image"
-import Link from "next/link"
+import Image from 'next/image';
+import Link from 'next/link';
 
-import { footerLinks } from "@/constants/constants"
-import { link } from "fs"
+import { footerLinks } from '@/constants/constants';
+import { link } from 'fs';
 
 function Footer() {
   return (
-    <footer className="flex flex-col 
+    <footer
+      className="flex flex-col 
     text-black-100 mt-5 border-t
-      border-grey-100">
-      <div className="flex max-md:flex-col
+      border-grey-100"
+    >
+      <div
+        className="flex max-md:flex-col
         flex-wrap justify-between gap-5 sm:px-16
-        px-6 py-10">
-        <div className="flex flex-col 
-          justify-start items-start gap-6">
-          <Image src='/logo.svg' alt="logo"
-            width={118} height={18} className="object-contain" />
-          <p className="text-base 
-            text-gray-700">
+        px-6 py-10"
+      >
+        <div
+          className="flex flex-col 
+          justify-start items-start gap-6"
+        >
+          <Image
+            src="/logo.svg"
+            alt="logo"
+            width={118}
+            height={18}
+            className="object-contain"
+          />
+          <p
+            className="text-base 
+            text-gray-700"
+          >
             Carhub 2024 <br />
             All rights reserved &copy;
-            </p>
+          </p>
         </div>
         <div className="footer__links">
-          {footerLinks.map(link => (
+          {footerLinks.map((link) => (
             <div key={link.title} className="footer__link">
               <h3>{link.title}</h3>
-              {link.links.map(item => (
+              {link.links.map((item) => (
                 <Link
                   id={item.title}
                   href={item.url}
@@ -39,22 +52,23 @@ function Footer() {
           ))}
         </div>
       </div>
-        <div className="flex justify-between
+      <div
+        className="flex justify-between
           items-center flex-wrap mt-10 border-t
-          border-gray-100 sm:px-16 px-6 py-10">
-          <p>@2024 CarHub. All Rights Reserved</p>
-          <div className="footer__copyrights-link">
-            <Link href='/' className="text-gray-500">
-              Privacy Policy
-            </Link>
-            <Link href='/' className="text-gray-500">
-              Terms of Use
-            </Link>
-          </div>
+          border-gray-100 sm:px-16 px-6 py-10"
+      >
+        <p className="pr-4">@2024 CarHub. All Rights Reserved</p>
+        <div className="footer__copyrights-link">
+          <Link href="/" className="text-gray-500">
+            Privacy Policy
+          </Link>
+          <Link href="/" className="text-gray-500">
+            Terms of Use
+          </Link>
         </div>
-
+      </div>
     </footer>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
